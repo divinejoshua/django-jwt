007cb52c433b4baa7c9abf4faa871faf5cc8ceb4
divinejoshua <divineuche399@gmail.com>
2022-08-14 13:42:41 +0100

    Started project

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ import uuid
 # from django.conf import settings
 # from django.db.models.signals import post_save
 # # from django.dispatch import receiver
-# from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.models import Token 
  
 
 class MyAccountManager(BaseUserManager):
