3b082d5e427780965889b8c4924ca436dd88c86e
divinejoshua <divineuche399@gmail.com>
2022-08-20 14:27:03 +0100

    Made changes

--- djangojwt/settings.py
+++ djangojwt/settings.py
@@ -104,9 +104,9 @@ REST_FRAMEWORK = {
 
 # JWT settings 
 REST_USE_JWT = True
-JWT_AUTH_COOKIE = 'jwt-auth'
-JWT_AUTH_HTTPONLY = True 
-JWT_AUTH_REFRESH_COOKIE = 'jwt-refresh-token'
+# JWT_AUTH_COOKIE = 'jwt-auth'
+# JWT_AUTH_HTTPONLY = True 
+# JWT_AUTH_REFRESH_COOKIE = 'jwt-refresh-token'
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
